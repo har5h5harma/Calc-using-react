9606e8a8a852a51f14f5da614d152fbfa3f4be1e
Harsh Sharma <hsharma7985@gmail.com>
2024-12-16 15:48:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [value, setValue] = useState('')
+
+  return (
+    <>
+    <div className="container">
+      <div className='calculator'>
+      <form action=''>
+        <div>
+          <input type='text' value={value} readOnly></input>
+        </div>
+        <div>
+          <input type="text" value={"AC"} onClick={e=> setValue("")}/>
+          <input type="text" value={"DEL"} onClick={e=> setValue((value.slice(0,-1)))}/>
+          <input type="text" value={"."} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"/"} onClick={e=> setValue(value+e.target.value)}/>
+        </div>
+        <div>
+          <input type="text" value={"7"} onClick={e=> setValue(value+e.target.value)} />
+          <input type="text" value={"8"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"9"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"*"} onClick={e=> setValue(value+e.target.value)}/>
+        </div>
+        <div>
+          <input type="text" value={"4"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"5"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"6"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"-"} onClick={e=> setValue(value+e.target.value)}/>
+        </div>
+        <div>
+          <input type="text" value={"1"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"2"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"3"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"+"} onClick={e=> setValue(value+e.target.value)}/>
+        </div>
+        <div>
+          <input type="text" value={"0"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"00"} onClick={e=> setValue(value+e.target.value)}/>
+          <input type="text" value={"="}  onClick={e=>setValue(eval(value))}/>
+        </div>
+      </form>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
